c7ab21f972f8148cf5f413c30e9f5cfed864c53e
alvin-kelvin <110433233+alvin-kelvin@users.noreply.github.com>
2024-07-30 23:33:08 +0100

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '67a782d0-474e-4b99-a219-9aa574a84cdd';
 export const MODAL_CLOSE_SEC = 2.5;
